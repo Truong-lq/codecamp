dae3cceccd4299b2df06eaf34300fd6532b8bd2a
Truong-lq <truonglq@zigexn.vn>
2025-01-03 15:03:04 +0700

    feat: add book detail page (#14)
    * Work has started on the feat/book_detail
    
    * feat: add book detail page
    
    * fix EOL

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,30 @@ class BooksController < ApplicationController
     books = Book.search_by_title_content(params[:query]).limit 12
     render partial: "book_list", locals: { books: books }
   end
+
+  def index
+    @books = Book.limit 12
+  end
+
+  def show
+    @book = Book.find_by id: params[:id]
+    @review = @book.reviews.build
+  end
+
+  def create_review
+    @book = Book.find_by id: params[:book_id]
+    @review = @book.reviews.build review_params
+
+    if @review.save
+      redirect_to book_path(@book)
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit :content
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_25_015307) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_27_022004) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "book_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 end
